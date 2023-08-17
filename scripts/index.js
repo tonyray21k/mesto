8a41d5b7152f08337fcacc261949610940c58aae
Tonyray <tonch4ik@gmail.com>
2023-08-17 16:01:37 +0500

    second_change

--- scripts/index.js
+++ scripts/index.js
@@ -37,7 +37,6 @@ const buttonCloseEditProfile = popupEditProfile.querySelector(".popup__close-but
 const popupInfo = popupEditProfile.querySelector(".popup__text-info");
 const elementForm = popupEditProfile.querySelector(".popup__fields");
 const popupNameEditProfile = popupEditProfile.querySelector(".popup__text-name");
-const form = popupEditProfile.querySelector("#popup-form");
 const popupAdd = document.querySelector("#add-popup");
 const elementImg = popupAdd.querySelector(".popup__text-img");
 const formAdd = popupAdd.querySelector("#addform");
@@ -61,7 +60,7 @@ function closePopup(popup) {
 }
 
 
-function handleFormSubmit(evt) {
+function FormEditProfile(evt) {
   evt.preventDefault();
   info.textContent = popupInfo.value;
   profileName.textContent = popupNameEditProfile.value;
@@ -70,7 +69,7 @@ function handleFormSubmit(evt) {
 function editPopupOpen() {
   popupNameEditProfile.value = profileName.textContent;
   popupInfo.value = info.textContent;
-  popupEditProfile.classList.add('popup_opened');
+  openPopup(popupEditProfile);
 }
 
 function addElement(evt) {
@@ -84,15 +83,15 @@ const renderElements = (name, link) => {
   elements.prepend(createElement(name, link));
 }
 
-function returnElement(name, link) {
-  const cardElement = elementsTemplate.querySelector('.elements__element').cloneNode(true);
-  cardElement.querySelector('.elements__card-name').textContent = name;
-  cardElement.querySelector('.elements__pic').alt = name;
-  cardElement.querySelector('.elements__pic').src = link;
-  return cardElement;
-}
-
 function createElement(name, link) {
+  function returnElement(name, link) {
+    const cardElement = elementsTemplate.querySelector('.elements__element').cloneNode(true);
+    cardElement.querySelector('.elements__card-name').textContent = name;
+    cardElement.querySelector('.elements__pic').alt = name;
+    cardElement.querySelector('.elements__pic').src = link;
+    return cardElement;
+  }
+  
   const cardElement = returnElement(name, link);
 
   const likeButton = cardElement.querySelector('.elements__card-button');
@@ -136,4 +135,4 @@ elements.prepend.apply(elements, defaultCards);
 
 buttonEditProfile.addEventListener('click', editPopupOpen)
 formAdd.addEventListener('submit', addElement);
-elementForm.addEventListener('submit', handleFormSubmit);
+elementForm.addEventListener('submit', FormEditProfile);
